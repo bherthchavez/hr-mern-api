46cb63b998ff907c296ed8f8459b6f014c31f095
bherth <bherthchavez@gmail.com>
2023-03-21 22:35:34 +0800

    updated url origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'https://hr-mern-frontend.onrender.com'
-    // 'http://localhost:3000'
+    //'https://hr-mern-frontend.onrender.com'
+    'http://localhost:3000'
 ]
 
 module.exports = allowedOrigins
